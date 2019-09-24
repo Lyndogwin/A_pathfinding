22b6c967279207920c84c858a439f329b9920cdd
Lyndogwin <brand949@gmail.com>
2019-09-24 14:34:01 -0400

    encountering problem with references

--- Assets/scripts/tileautomate.cs
+++ Assets/scripts/tileautomate.cs
@@ -13,29 +13,41 @@ public class tileautomate : MonoBehaviour
         public int y; 
     }
      
-    public struct Node
+    public struct Node : IEquatable<Node>
     {
         // *** implement enumerated state or use integer representations
         
         public Tile cur; // will reprsent state
         public Tile pointer; 
         public Pos parent;
+        public Pos myPos;
         public int f;
         public int g;
         public int h;
-        public Node(int x, int y, Tile t, Tile s)
+        public Node(int w, int r, Tile t, Tile s)
         {
-            parent.x = x;
-            parent.y = y;
             cur = t;
             pointer = s;
-            f = 0;
+            myPos.x = w;
+            myPos.y = r;
+            parent.x = 500;
+            parent.y = 500;
+            f = 500;
             g = 0;
             h = 0;
         }
+        public bool Equals(Node node)
+            => node.myPos.x == myPos.x && node.myPos.y == myPos.y;
+    }
+    // create references 
+    public class Ref<T> where T : struct
+    {
+        public T Value {get; set;}
     }
 
     private Node[,] terrainmap;
+    private List<Ref<Node>> openList = new List<Ref<Node>>();
+    private List<Ref<Node>> closedList = new List<Ref<Node>>();
     private Vector3Int startPos;
     private Vector3Int endPos;
     private Vector3Int curPos;
@@ -88,10 +100,10 @@ public class tileautomate : MonoBehaviour
     bool gameStart = true;
     bool startSelected = false;
     bool endSelected = false;
+    bool pathFound = false;
 
     public void genMap()
     {
-        // clearmap(false);
         width = tmapsize.x;
         height = tmapsize.y;
         numTiles = width * height;
@@ -104,7 +116,7 @@ public class tileautomate : MonoBehaviour
         {
             for(int j = 0; j < height; j++)
             {
-                terrainmap[i,j] = new Node(0,0,std,none);
+                terrainmap[i,j] = new Node(i,j,std,none);
             }
         }
         
@@ -150,7 +162,7 @@ public class tileautomate : MonoBehaviour
     {
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int tilePos = tileGrid.WorldToCell(worldPos);
-        Debug.Log(tilePos);
+        //Debug.Log(tilePos);
         if(gameStart)
         {
             if(!startSelected)
@@ -190,68 +202,179 @@ public class tileautomate : MonoBehaviour
                         {curPos.x,curPos.y -1}, {curPos.x +1,curPos.y -1}, 
                         {curPos.x +1,curPos.y}, {curPos.x +1,curPos.y +1}, 
                         {curPos.x,curPos.y +1}, {curPos.x -1,curPos.y+1}};
-        for(int i = 0; i < width; i++)
+
+        for(int k = 0; k < 8; k++)
         {
-            for(int j = 0; j < height; j++)
+            int i = moves[k,0];
+            int j = moves[k,1];
+            bool diag_wall = false;
+            if(i >= width || j >= height || i < 0 || j < 0){
+                continue;
+            }
+            if(terrainmap[i,j].cur == wall || closedList.Exists(p => p.Value.Equals(terrainmap[i,j])))
             {
-                for(int k = 0; k < 8; k++)
-                {
-                    if((i == moves[k,0]) && (j == moves[k,1]))  
+                continue;
+            }
+            
+            switch(k)
+            {
+                case 0:
+                    break;
+                case 1:
+                    if(terrainmap[moves[k-1,0],moves[k-1,1]].cur == wall || terrainmap[moves[k+1,0],moves[k+1,1]].cur == wall)
                     {
-                        terrainmap[i,j].parent.x = curPos.x;
-                        terrainmap[i,j].parent.y = curPos.y;
-                        switch(k)
-                        {
-                            case 0:
-                                terrainmap[i,j].pointer = right;
-                                terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + s_move;
-                                break;
-                            case 1:
-                                terrainmap[i,j].pointer = up_right;
-                                terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + d_move;
-                                break;
-                            case 2:
-                                terrainmap[i,j].pointer = up;
-                                terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + s_move;
-                                break;
-                            case 3:
-                                terrainmap[i,j].pointer = up_left;
-                                terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + d_move;
-                                break;
-                            case 4:
-                                terrainmap[i,j].pointer = left;
-                                terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + s_move;
-                                break;
-                            case 5:
-                                terrainmap[i,j].pointer = down_left;
-                                terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + d_move;
-                                break;
-                            case 6:
-                                terrainmap[i,j].pointer = down;
-                                terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + s_move;
-                                break;
-                            case 7:
-                                terrainmap[i,j].pointer = down_right;
-                                terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + d_move;
-                                break;
-                        }
-                        int e_dx = Math.Abs(endPos.x - i); 
-                        int e_dy = Math.Abs(endPos.y - j);
-                        terrainmap[i,j].h = e_dx + e_dy;
-                        terrainmap[i,j].f = terrainmap[i,j].g + terrainmap[i,j].h;
-                        Debug.Log("current position:"+ curPos + i + " " + j );
-                        Debug.Log("h value is: " + terrainmap[i,j].h);
-                        Debug.Log("g value is: " + terrainmap[i,j].g);
-                        Debug.Log("f value is: " + terrainmap[i,j].f);
+                        diag_wall = true;
                     }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    if(terrainmap[moves[k-1,0],moves[k-1,1]].cur == wall || terrainmap[moves[k+1,0],moves[k+1,1]].cur == wall)
+                    {
+                        diag_wall = true;
+                    }
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    if(terrainmap[moves[k-1,0],moves[k-1,1]].cur == wall || terrainmap[moves[k+1,0],moves[k+1,1]].cur == wall)
+                    {
+                        diag_wall = true;
+                    }
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    if(terrainmap[moves[k-1,0],moves[k-1,1]].cur == wall || terrainmap[moves[0,0],moves[0,1]].cur == wall)
+                    {
+                        diag_wall = true;
+                    }
+                    break;
+            }
+            if(diag_wall)
+            {
+                continue;
+            }
+            
+            //Debug.Log("<color=red>"+(!openList.Exists(p => p.Value.Equals(terrainmap[i,j])))+"</color>");
+            if((terrainmap[curPos.x,curPos.y].cur == start) || (!openList.Exists(p => p.Value.Equals(terrainmap[i,j]))) ||
+                (terrainmap[curPos.x,curPos.y].g < terrainmap[terrainmap[i,j].parent.x, terrainmap[i,j].parent.y].g))
+            {
+                terrainmap[i,j].parent.x = curPos.x;
+                terrainmap[i,j].parent.y = curPos.y;
+                if(!openList.Exists(p => p.Value.Equals(terrainmap[i,j])))
+                {
+                    Debug.Log("<color=yellow> Alert: </color>Adding a new node to openList");
+                    openList.Add(new Ref<Node> {Value = terrainmap[i,j]}); // add if not in openList
+                }
+                switch(k)
+                {
+                    case 0:
+                        terrainmap[i,j].pointer = right;
+                        terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + s_move;
+                        break;
+                    case 1:
+                        terrainmap[i,j].pointer = up_right;
+                        terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + d_move;
+                        break;
+                    case 2:
+                        terrainmap[i,j].pointer = up;
+                        terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + s_move;
+                        break;
+                    case 3:
+                        terrainmap[i,j].pointer = up_left;
+                        terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + d_move;
+                        break;
+                    case 4:
+                        terrainmap[i,j].pointer = left;
+                        terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + s_move;
+                        break;
+                    case 5:
+                        terrainmap[i,j].pointer = down_left;
+                        terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + d_move;
+                        break;
+                    case 6:
+                        terrainmap[i,j].pointer = down;
+                        terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + s_move;
+                        break;
+                    case 7:
+                        terrainmap[i,j].pointer = down_right;
+                        terrainmap[i,j].g = terrainmap[curPos.x,curPos.y].g + d_move;
+                        break;
+                }
+                /* 
+                int e_dx = Math.Abs(endPos.x - i); 
+                int e_dy = Math.Abs(endPos.y - j);
+                terrainmap[i,j].h = (e_dx + e_dy)*10;
+                terrainmap[i,j].f = terrainmap[i,j].g + terrainmap[i,j].h;
+                Debug.Log("current position:"+ curPos + i + " " + j );
+                Debug.Log("h value is: " + terrainmap[i,j].h);
+                Debug.Log("g value is: " + terrainmap[i,j].g);
+                Debug.Log("f value is: " + terrainmap[i,j].f);
+                Debug.Log("h copy value is: " + openList[openList.Count - 1].h);
+                Debug.Log("g copy value is: " + openList[openList.Count - 1].g);
+                Debug.Log("f copy value is: " + openList[openList.Count - 1].f);
+                */
+            }
+        }
+        if(curPos.x == startPos.x && curPos.y == startPos.y)
+        {
+            terrainmap[curPos.x,curPos.y].cur = start_s;
+        }
+        else if(curPos.x == endPos.x && curPos.y == endPos.y)
+        {
+            terrainmap[curPos.x,curPos.y].cur = end_s;
+            pathFound = true;
+        }
+        else
+        {
+            terrainmap[curPos.x,curPos.y].cur = select;
+        }
+
+        closedList.Add(new Ref<Node> {Value = terrainmap[curPos.x,curPos.y]});
+        
+        foreach (Ref<Node> i in openList)
+        {   
+            //if(i.Value.myPos.x == curPos.x && i.Value.myPos.y == curPos.y )
+            if(i.Value.Equals(terrainmap[curPos.x,curPos.y]))
+            {    
+                openList.Remove(i);
+                //Debug.Log("<color=green>Success! </color>removed current node from open list");
+            }
+        }
+        
+       
+        if(terrainmap[curPos.x,curPos.y].cur != end_s) //may be problematic
+        {
+            Ref<Node> lowest_f = openList[0];//new Node(500,500,none,none); this may be the problem
+            
+            // loop through the openList of structs "Node" and calculate the f value
+            foreach (Ref<Node> i in openList)
+            {
+                int e_dx = Math.Abs(endPos.x - i.Value.myPos.x); 
+                int e_dy = Math.Abs(endPos.y - i.Value.myPos.y);
+                terrainmap[i.Value.myPos.x,i.Value.myPos.y].h = (e_dx + e_dy)*10;
+                terrainmap[i.Value.myPos.x,i.Value.myPos.y].f = terrainmap[i.Value.myPos.x,i.Value.myPos.y].g + terrainmap[i.Value.myPos.x,i.Value.myPos.y].h;
+                Debug.Log("<color=green>f here is "+ i.Value.f+"</color>"); //references are working if changes are noticable
+                Debug.Log("position is "+ i.Value.myPos.x + "," + i.Value.myPos.y);
+                Debug.Log("parent position is "+ i.Value.parent.x + "," + i.Value.parent.y);
+                if(i.Value.f <= lowest_f.Value.f)
+                /* 
+                
+                */
+                {
+                    lowest_f = i;
                 }
             }
+            //Debug.Log("lowest f is " + lowest_f.Value.f);
+            Debug.Log("---------------------------");
+            curPos.x = lowest_f.Value.myPos.x;
+            curPos.y = lowest_f.Value.myPos.y;
         }
     }
     
     void Start()
     {
-        //Camera.main.orthograpthicSize = widthToBeSeen * Screen.height / Screen.width * 0.5;
         genMap();
     }
     
@@ -259,7 +382,7 @@ public class tileautomate : MonoBehaviour
     void Update()
     {
         getStartandEnd();
-        if(!gameStart)
+        if(!gameStart && !pathFound)
         {
             search();
         }
